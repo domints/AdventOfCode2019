2dfe7ae4c3b776905709df5576238336aa645fb2
Dominik Szymański <domints@gmail.com>
2019-12-10 10:47:50 +0100

    Add Day 9 sample input

--- Day9/Part1/Program.cs
+++ Day9/Part1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day9
 {
@@ -6,7 +9,19 @@ namespace Day9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start");
+            var input = File.ReadAllText("input.txt")
+                .Split(',')
+                .Select(l => l.Trim())
+                .Where(l => !string.IsNullOrWhiteSpace(l))
+                .Select(l => long.Parse(l))
+                .ToList();
+            
+            Console.WriteLine("Loaded input");
+
+            var comp = new Computer(input);
+            comp.Run();
+            Console.WriteLine(string.Join(',', comp.Memory));
         }
     }
 }
